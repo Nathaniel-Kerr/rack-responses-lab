cad5f9a75e8d81db56bdcb9723725faa144993e6
Nathaniel_Kerr <NathanielKerr10@gmail.com>
2021-02-26 13:45:30 -0800

    Done.

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+    def call(env)
+        #env[:timestamp] = Time.now
+        resp = Rack::Response.new
+        time = Time.now.hour
+        puts time
+        if time <= 12
+            resp.write "Good Morning!"
+        elsif time > 12 
+            resp.write "Good Afternoon!"
+        end
+        resp.finish 
+
+    end
+end 
